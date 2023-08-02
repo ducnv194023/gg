f5f25902c31e9df032362e1b78aa282b2f011b8f
duc <duc@ducs-MacBook-Pro.local>
2023-08-03 03:10:37 +0700

    fix

--- ggsheet/app.js
+++ ggsheet/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const axios = require('axios').default()
 const port = 3000
 app.get('/', (req, res) => {
     const api_key = 'a51b91d336ef4697ad7a84812b359dac'
@@ -9,8 +10,20 @@ app.get('/', (req, res) => {
 
     res.redirect(string)
 })
-app.get('/sapo', (req, res) => {
-  res.send('Hello World!')
+app.get('/sapo', async (req, res) => {
+  const code = req.params.code
+  const response = await axios({
+    method: 'post',
+    url: 'https://planetcosmetic.mysapogo.com/admin/oauth/access_token',
+    data: {
+      clientId: 'clientid',
+      client_secret: 'client_secret',
+      code
+    }
+  });
+  const access_token = response.access_token
+  console.log(access_token)
+
 })
 
 app.listen(port, () => {
